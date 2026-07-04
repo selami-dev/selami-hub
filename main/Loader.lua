a8074a67d207f542a90b1ed6076a61f1fee6ec60
selami-dev <selamihubgithub@gmail.com>
2026-07-04 19:36:21 +0300

    Update Loader.lua

--- main/Loader.lua
+++ main/Loader.lua
@@ -5,8 +5,8 @@
     local Args = {
         Key = "LexinKocaMemeleri",
         ForceUniversal = false,
-		HideConsole = true,
-		NoGui = true,
+		HideConsole = false,
+		NoGui = false,
     }
     
     local SelamiHub = loadstring(game:HttpGet("https://raw.githubusercontent.com/selami-dev/selami-hub/refs/heads/main/main/Loader.lua"))()(Args)
